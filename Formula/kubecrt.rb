4f5e235ae4b3dec6ac9c584250198f60afa75464
Josef Janda <josef.janda@wandera.com>
2021-02-05 17:27:20 +0100

    remove dep dependency

--- Formula/kubecrt.rb
+++ Formula/kubecrt.rb
@@ -5,8 +5,7 @@ class Kubecrt < Formula
       tag:      "v0.9.1",
       revision: "c8bc60b2cf3ebc9a9e4d07d5076733c3c5e40d1d"
   head "https://github.com/wandera/kubecrt.git"
-
-  depends_on "dep" => :build
+  
   depends_on "go" => :build
 
   def install
